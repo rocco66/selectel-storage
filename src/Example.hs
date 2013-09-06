a6f4c5d16f15dda9d3b6c1f391e718d7699f30fa
Maxim Mitroshin <rocco66max@gmail.com>
2013-09-06 12:17:39 +0400

    Init commit

---
+++ src/Example.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+
+module Swift.Example (test) where
+
+import Control.Monad.IO.Class (liftIO)
+import Data.ByteString (ByteString)
+
+import Swift
+
+data SelcdnAuth = SelcdnAuth
+    { selcdnAuthAccount :: ByteString
+    , selcdnAuthKey     :: ByteString
+    } deriving (Show, Eq)
+
+data SwiftConnectInfo = SwiftConnectInfo
+    { swiftConnectInfoStorageUrl :: {-# UNPACK #-} !ByteString
+    , swiftConnectToken          :: {-# UNPACK #-} !ByteString }
+  deriving (Eq, Show)
+
+instance SwiftAuthenticator SelcdnAuth SwiftConnectInfo where
+    mkRequestAuthInfo SelcdnAuth { .. } =
+        addHeaders [ ("X-Auth-User", selcdnAuthAccount)
+                   , ("X-Auth-Key", selcdnAuthKey) ]
+    getStorageUrl = swiftConnectInfoStorageUrl
+    prepareRequest SwiftConnectInfo { .. } =
+        addHeader ("X-Auth-Token", swiftConnectToken)
+    mkInfoState resp = do
+        swiftConnectInfoStorageUrl <- findHeaderInResponse resp "X-Storage-Url"
+        swiftConnectToken <- findHeaderInResponse resp "X-Storage-Token"
+        return SwiftConnectInfo { .. }
+
+test :: IO ()
+test = let
+    authentificator = SelcdnAuth { selcdnAuthAccount = "7091_hswift"
+                                 , selcdnAuthKey     = "bh18Px6zxg" }
+    -- authUrl = "https://auth.selcdn.ru" in
+    authUrl = "https://auth.selcdn.ru" in
+    runSwift authUrl authentificator $
+        getContainer "hswift" >>= liftIO . putStrLn . show
